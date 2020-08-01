e2b806056c6c006663b1f489e4409b4f3b2cbe56
Adam Hammes <ahammes@cortexmedia.ca>
2020-08-01 12:03:43 -0400

    refactor: Simplify upload implementation

--- geocoding_cache/backup.py
+++ geocoding_cache/backup.py
@@ -1,5 +1,4 @@
 import datetime
-import io
 import sqlite3
 import tempfile
 
@@ -8,6 +7,11 @@ import boto3
 
 def backup_db(source: sqlite3.Connection):
     print("Backing up the database")
+    s3_client = boto3.client("s3")
+
+    day_of_month = datetime.datetime.today().day
+    object_name = f"geocoding_cache/{day_of_month:02}.sqlite3"
+
     with tempfile.NamedTemporaryFile() as destination_file:
         dest = sqlite3.connect(destination_file.name)
         print("Making a copy of the database...")
@@ -15,13 +19,8 @@ def backup_db(source: sqlite3.Connection):
 
         dest.close()
 
-        in_memory_db = io.BytesIO(destination_file.read())
+        print("Uploading to s3...")
+        s3_client.upload_file(destination_file.name, "kijiji-apartments", object_name)
+        print("...done")
 
-    s3_client = boto3.client("s3")
 
-    day_of_month = datetime.datetime.today().day
-    object_name = f"geocoding_cache/{day_of_month:02}.sqlite3"
-
-    print("Uploading to s3...")
-    s3_client.upload_fileobj(in_memory_db, "kijiji-apartments", object_name)
-    print("...done")
